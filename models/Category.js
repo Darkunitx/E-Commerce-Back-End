ba0a48a9860df04dc1d9aa9a6ea112d3458355e7
Darkunitx <darkunitx@live.com>
2023-03-02 16:23:12 -0600

    routes added for category, however put still needs to be fixed

--- models/Category.js
+++ models/Category.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
@@ -11,11 +10,11 @@ Category.init(
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
-  },
+    },
     category_name: {
       type: DataTypes.STRING,
       allowNull: false,
-  },
+    },
   },
   {
     sequelize,
